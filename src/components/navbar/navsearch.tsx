04d61612deda4c86192f1ac25cefa728feacb09c
cisu <georgrekis@gmail.com>
2024-06-24 16:01:40 +0300

    work with header

--- src/components/navbar/navsearch.tsx
+++ src/components/navbar/navsearch.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Input } from "../ui/input";
 
 const Navsearch = () => {
   return (
-    <div>Navsearch</div>
-  )
-}
+    <Input
+      type="search"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted "
+    />
+  );
+};
 
-export default Navsearch
+export default Navsearch;
