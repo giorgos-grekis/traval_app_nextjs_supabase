39232dc7cf26ce58482ed44692ee0ca8c473d8f8
cisu <georgrekis@gmail.com>
2024-06-24 18:20:32 +0300

    remove testing git

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,5 +1,4 @@
 "use client";
-// test
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
