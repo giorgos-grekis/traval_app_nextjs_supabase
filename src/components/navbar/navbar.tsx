32e9726a9ca649c49955cfbc2dc34424f57295ee
cisu <georgrekis@gmail.com>
2024-06-24 15:54:06 +0300

    work with navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import DarkMode from "./dark-mode";
+import LinksDropdown from "./links-dropdown";
+import Logo from "./logo";
+import Navsearch from "./navsearch";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="border-b">
+      <div className="container flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <Navsearch />
+        <div className="flex gap-4 items-center ">
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
