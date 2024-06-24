d8a486beb32787059fadd71d7f935dbdaed5cc15
cisu <georgrekis@gmail.com>
2024-06-24 15:55:48 +0300

    add nav bar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Travel App",
-  description: "Discover your next adventure with our travel app! Plan trips, book flights, find accommodations, and explore new destinations effortlessly. Start your journey today and experience the world like never before.",
+  description:
+    "Discover your next adventure with our travel app! Plan trips, book flights, find accommodations, and explore new destinations effortlessly. Start your journey today and experience the world like never before.",
 };
 
 export default function RootLayout({
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        <Navbar />
+        <main className="container py-10">{children}</main>
+      </body>
     </html>
   );
 }
