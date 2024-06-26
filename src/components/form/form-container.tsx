c87ed569811a718392ca5f4929f655744479bcef
cisu <georgrekis@gmail.com>
2024-06-26 21:22:28 +0300

    add a form

---
+++ src/components/form/form-container.tsx
@@ -0,0 +1,29 @@
+"use client";
+import type { PropsWithChildren } from "react";
+
+import { useFormState } from "react-dom";
+import { useEffect } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { actionFunction } from "@/utils/types";
+
+const initialState = {
+  message: "",
+};
+
+const FormContainer = ({
+  action,
+  children,
+}: PropsWithChildren<{ action: actionFunction }>) => {
+  const [state, formAction] = useFormState(action, initialState);
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (state.message) {
+      toast({ description: state.message });
+    }
+  }, [state]);
+
+  return <form action={formAction}>{children}</form>;
+};
+
+export default FormContainer;
